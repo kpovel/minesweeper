4ad8e80f96f96cbda25312ab825cef855c9a9a27
Pavlo Karas <thepavlokaras@gmail.com>
2024-07-01 06:46:40 +0300

    refactor: 2d array

--- lib/mine_map.ex
+++ lib/mine_map.ex
@@ -8,29 +8,50 @@ defmodule MineMap do
       when is_integer(rows)
       when is_integer(cols)
       when is_integer(mines) do
-    assert((rows * cols) / 2 > mines, "At least half of the cells should not be mines")
+    assert(rows * cols / 2 > mines, "At least half of the cells should not be mines")
 
+    generate_map(rows, cols, mines)
+  end
+
+  defp generate_map(rows, cols, mines) do
     default_map = List.duplicate(:clear, rows * cols)
-    map = generate_map(rows, cols, mines, default_map)
-    assert Enum.count(map, fn el -> el == :mine end) == mines
+    map = flat_map(rows, cols, mines, default_map)
+
+    map =
+      Enum.map(0..(rows - 1), fn i ->
+        Enum.slice(map, cols * i, cols)
+      end)
+
+    assert Enum.count(map) == rows
+
+    Enum.each(map, fn r ->
+      assert Enum.count(r) == cols
+    end)
+
+    map_mines =
+      Enum.map(map, fn col -> Enum.count(col, fn c -> c == :mine end) end)
+      |> Enum.sum()
+
+    assert map_mines == mines
+
     map
   end
 
-  def generate_map(rows, cols, remind_mines, map) do
+  defp flat_map(rows, cols, remind_mines, map) do
     assert remind_mines > 0
     {row, col} = {Enum.random(0..(rows - 1)), Enum.random(0..(cols - 1))}
     target_index = cols * row + col
 
     case Enum.at(map, target_index) do
       :mine ->
-        generate_map(rows, cols, remind_mines, map)
+        flat_map(rows, cols, remind_mines, map)
 
       :clear ->
         map = List.replace_at(map, target_index, :mine)
 
         case remind_mines do
           1 -> map
-          remind -> generate_map(rows, cols, remind - 1, map)
+          remind -> flat_map(rows, cols, remind - 1, map)
         end
     end
   end
@@ -39,25 +60,26 @@ end
 defmodule MineMap.Format do
   alias IEx.Helpers
 
-  def print_map(map, rows, cols) do
+  def print_map(map) do
     Helpers.clear()
+    cols = hd(map) |> Enum.count()
     print_header(cols)
-    print_body(map, rows, cols)
+    print_body(map)
   end
 
-  defp print_body(map, rows, cols) do
+  defp print_body(map) do
+    cols = hd(map) |> Enum.count()
+
     row_space = Integer.to_string(cols) |> String.length()
     row_space = row_space + 2
 
-    Enum.each(0..(rows - 1), fn i ->
+    Enum.with_index(map, fn row, i ->
       row_number = "#{[65 + i]} "
       IO.write(row_number)
-
       leading_space = div(row_space + 1, 2)
       trailing_space = leading_space + 1
 
-      Enum.slice(map, i * cols, cols)
-      |> Enum.map(fn cell ->
+      Enum.map(row, fn cell ->
         case cell do
           :mine -> "X"
           :clear -> " "
